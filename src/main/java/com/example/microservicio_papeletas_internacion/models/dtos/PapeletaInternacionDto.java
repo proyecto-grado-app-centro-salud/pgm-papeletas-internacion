9949e6276211fa1df2f32e9dddfc1614a4155034
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-10-14 10:39:56 -0400

    [Fix] agregando campos en obtencion  papeletas de internacion

--- src/main/java/com/example/microservicio_papeletas_internacion/models/dtos/PapeletaInternacionDto.java
+++ src/main/java/com/example/microservicio_papeletas_internacion/models/dtos/PapeletaInternacionDto.java
@@ -20,8 +20,14 @@ public class PapeletaInternacionDto {
     private Date updatedAt;
     private Date deletedAt;
     private Integer idHistoriaClinica;
+    private String diagnosticoPresuntivo;
+    private Integer idEspecialidad;
+    private String nombreEspecialidad;
     private Integer idMedico;
     private String nombreMedico;
+    private Integer idPaciente;
+    private String pacientePropietario;
+    private String ciPropietario;
 
     public PapeletaInternacionDto convertirPapeletaInternacionEntityAPapeletaInternacionDto(PapeletaInternacionEntity papeletaInternacionEntity) {
         PapeletaInternacionDto dto = new PapeletaInternacionDto();
@@ -32,10 +38,18 @@ public class PapeletaInternacionDto {
         dto.setUpdatedAt(papeletaInternacionEntity.getUpdatedAt());
         dto.setDeletedAt(papeletaInternacionEntity.getDeletedAt());
         dto.setIdHistoriaClinica(papeletaInternacionEntity.getHistoriaClinica().getIdHistoriaClinica());
+        dto.setDiagnosticoPresuntivo(papeletaInternacionEntity.getHistoriaClinica().getDiagnosticoPresuntivo());
+        dto.setIdEspecialidad(papeletaInternacionEntity.getHistoriaClinica().getEspecialidad().getIdEspecialidad());
+        dto.setNombreEspecialidad(papeletaInternacionEntity.getHistoriaClinica().getEspecialidad().getNombre());
         dto.setIdMedico(papeletaInternacionEntity.getMedico().getIdUsuario());
         dto.setNombreMedico(papeletaInternacionEntity.getMedico().getNombres() + " " + 
                            papeletaInternacionEntity.getMedico().getApellidoPaterno() + " " + 
                            papeletaInternacionEntity.getMedico().getApellidoMaterno());
+        dto.setIdPaciente(papeletaInternacionEntity.getHistoriaClinica().getPaciente().getIdUsuario());
+        dto.setPacientePropietario(papeletaInternacionEntity.getHistoriaClinica().getPaciente().getNombres() + " " +
+                                   papeletaInternacionEntity.getHistoriaClinica().getPaciente().getApellidoPaterno() + " " +        
+                                   papeletaInternacionEntity.getHistoriaClinica().getPaciente().getApellidoMaterno());
+        dto.setCiPropietario(papeletaInternacionEntity.getHistoriaClinica().getPaciente().getCi());
         return dto;
     }
 }
